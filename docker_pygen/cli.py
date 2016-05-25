1053dde7eb565848c7db5f61e3f0c9ef9499d189
Marc Brinkmann <git@marcbrinkmann.de>
2016-05-25 16:49:49 +0200

    Added proper functionality for outputting.

--- docker_pygen/cli.py
+++ docker_pygen/cli.py
@@ -1,6 +1,8 @@
 from functools import partial
+import sys
 
 import click
+import jinja2
 from docker.client import Client
 
 DEFAULT_URL = 'unix://var/run/docker.sock'
@@ -10,14 +12,34 @@ info = partial(click.echo, err=True)
 # helpful: https://docs.docker.com/engine/reference/api/images/event_state.png
 
 
-def update_configurations(cl, events=[]):
+def update_configurations(cl, template, output_file, events=[]):
     containers = cl.containers(all=True)
     images = cl.images()
     container_details = {id: cl.inspect_container(c['Id']) for c in containers}
-    images_details = {id: cl.inspect_image(i['Id']) for i in images}
+    image_details = {id: cl.inspect_image(i['Id']) for i in images}
 
     info('Collected {} running containers and {} images'.format(
         len(containers), len(images)))
+
+    env = jinja2.Environment()
+
+    with open(template) as tpl_src:
+        tpl = env.from_string(tpl_src.read())
+
+        info('Compiled template {}'.format(template))
+        result = tpl.render(containers=containers,
+                            images=images,
+                            container_details=container_details,
+                            image_details=image_details)
+
+    info('Successfully rendered template {}'.format(template))
+
+    outp = open(output_file, 'w') if output_file else sys.stdout
+
+    with outp as out:
+        out.write(result)
+
+    info('Wrote {}'.format(output_file or 'to stdout'))
 
 
 @click.command('docker-pygen')
@@ -26,7 +48,12 @@ def update_configurations(cl, events=[]):
               default=DEFAULT_URL,
               help='The url used to connect ot the docker server [default: ' +
               DEFAULT_URL + ']')
-def cli(url):
+@click.option('-o',
+              '--output-file',
+              help='Output directory for template files',
+              type=click.Path())
+@click.argument('template')
+def cli(url, template, output_file):
     # initialize Client
     cl = Client(base_url=url, version='auto')
 
@@ -35,4 +62,4 @@ def cli(url):
     info('Connected to Docker {v[Version]}, api version '
          '{v[ApiVersion]}.'.format(v=v))
 
-    update_configurations(cl)
+    update_configurations(cl, template, output_file)
